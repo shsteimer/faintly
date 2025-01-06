a9e678a295861137d1e27f524a75a8cfa965c08d
Sean Steimer <ssteimer@adobe.com>
2025-01-06 13:25:26 -0800

    accordion test changes

--- test/fixtures/blocks/accordion/accordion.js
+++ test/fixtures/blocks/accordion/accordion.js
@@ -1,8 +1,5 @@
 import { renderBlock } from '../../../../src/faintly.js';
 
 export default async function decorate(block) {
-  await renderBlock(block, {
-    getLabel: ({ item }) => item.items[0].childNodes,
-    getBody: ({ item }) => item.items[1].childNodes,
-  });
+  await renderBlock(block);
 }
