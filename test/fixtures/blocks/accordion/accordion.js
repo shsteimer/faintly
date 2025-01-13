75d296c9288d5a0af93a7b543d17574af1030183
Sean Steimer <ssteimer@adobe.com>
2025-01-13 10:22:33 -0800

    chore: add perf test to the code independant of perf code changes

--- test/fixtures/blocks/accordion/accordion.js
+++ test/fixtures/blocks/accordion/accordion.js
@@ -1,5 +1,25 @@
 import { renderBlock } from '../../../../src/faintly.js';
 
+// non-faintly decorator
+// used for perf comparison
+export function decorateOld(block) {
+  [...block.children].forEach((row) => {
+    // decorate accordion item label
+    const label = row.children[0];
+    const summary = document.createElement('summary');
+    summary.className = 'accordion-item-label';
+    summary.append(...label.childNodes);
+    // decorate accordion item body
+    const body = row.children[1];
+    body.className = 'accordion-item-body';
+    // decorate accordion item
+    const details = document.createElement('details');
+    details.className = 'accordion-item';
+    details.append(summary, body);
+    row.replaceWith(details);
+  });
+}
+
 export default async function decorate(block) {
   await renderBlock(block);
 }
--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,6 +1,16 @@
 export default {
-  files: [
-    'test/**/*.test.{html,js}',
+  groups: [
+    {
+      name: 'unit',
+      files: [
+        'test/**/*.test.{html,js}',
+        '!test/performance/**',
+      ],
+    },
+    {
+      name: 'perf',
+      files: 'test/performance/**/*.test.{html,js}',
+    },
   ],
   coverageConfig: {
     report: true,
